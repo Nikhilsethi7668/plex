bcf20c0dcaa69bcd3d3d212fedf52ad2472668ff
Nikhil Sethi <132539777+Nikhilsethi7668@users.noreply.github.com>
2025-01-14 02:42:20 +0530

    Update Api.jsx

--- frontend/src/Service/Api.jsx
+++ frontend/src/Service/Api.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000", // Base URL for backend
+    baseURL: "https://plexdubai1.onrender.com", // Base URL for backend
     headers: {
         "Content-Type": "application/json",
     },
